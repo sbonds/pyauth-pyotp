5ce38bb0b023d1ce857abad940f71bda7f693e40
Andrey Kislyuk <kislyuk@gmail.com>
2018-11-05 16:18:03 -0800

    flake8: ignore W504, unignore E305, E401, E226

--- src/pyotp/utils.py
+++ src/pyotp/utils.py
@@ -85,12 +85,12 @@ def _compare_digest(s1, s2):
         differences |= ord(c1) ^ ord(c2)
     return differences == 0
 
+
 try:
     # Python 3.3+ and 2.7.7+ include a timing-attack-resistant
     # comparison function, which is probably more reliable than ours.
     # Use it if available.
     from hmac import compare_digest
-
 except ImportError:
     compare_digest = _compare_digest
 
